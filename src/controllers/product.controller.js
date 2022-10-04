c2f206d53c11778a7517d3216fa4fd62fd0db5b4
Arturo Renteria <arthur_metal88@hotmail.com>
2022-10-03 22:49:56 -0500

    feat: Funciones CRUD

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,13 +10,48 @@ const getProducts = async (req, res)=>{
     }
 }
 
-const getProduct = async (req, res)=>{}
+const getProduct = async (req, res)=>{
+    try {
+        const query = await productServices.readProduct(req.params,res);
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
 
-const createProduct = async (req, res)=>{}
+const createProduct = async (req, res)=>{
+    try {
+        const { sku, name_product, price, description_product, url_image } = req.body;
+        const query = await productServices.createProduct(sku, name_product, price, description_product, url_image,res);
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
 
-const updateProduct = async (req, res)=>{}
+const updateProduct = async (req, res)=>{
+    try {
+        const {sku} = req.params;
+        const {name_product, price, description_product, url_image } = req.body;
+        const query = await productServices.updateProduct(sku, name_product, price, description_product, url_image,res);
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
 
-const deleteProduct = async (req, res)=>{}
+const deleteProduct = async (req, res)=>{
+    try {
+        const query = await productServices.deleteProduct(req.params,res);
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
 
 export const methods ={
     getProducts,
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -6,6 +6,41 @@ async function readProducts(response) {
     return response.json(result)
 }
 
+async function readProduct(body,response) {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT * FROM products WHERE sku = ?", body.sku)
+    return response.json(result)
+}
+
+async function createProduct(sku, name_product, price, description_product, url_image,response) {
+    if (sku === undefined || name_product === undefined || price === undefined || description_product === undefined || url_image === undefined ) {
+        return response.status(400).json({message:"Bad Request. Please fill all field."});
+    }
+        const connection = await getConnection();
+        const result = await connection.query("INSERT INTO `products` (`sku`, `name_product`, `price`, `description_product`, `url_image`) VALUES (?,?,?,?,?)", [sku,name_product,price,description_product,url_image])
+        return response.json(result)
+}
+
+// Listo
+async function updateProduct(sku, name_product, price, description_product, url_image,response) {
+    if (name_product === undefined || price === undefined || description_product === undefined || url_image === undefined ) {
+        return response.status(400).json({message:"Bad Request. Please fill all field."});
+    }
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE products SET `name_product`= ?,`price`=?,`description_product`=?,`url_image`=? WHERE sku= ?", [name_product, price, description_product, url_image,sku]);
+        return response.json(result)
+}
+
+async function deleteProduct(body,response) {
+    const connection = await getConnection();
+    const result = await connection.query("DELETE FROM products WHERE sku = ?", body.sku)
+    return response.json(result)
+}
+
 export const methods = {    
-    readProducts,    
+    readProducts,
+    readProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct    
 }

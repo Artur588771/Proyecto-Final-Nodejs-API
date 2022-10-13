2608824b7f381336307381d48bfda2fcd4bef159
Arturo Renteria <arthur_metal88@hotmail.com>
2022-10-12 20:14:18 -0500

    feat: Preparando para Heroku

--- src/common/constants.js
+++ src/common/constants.js
@@ -5,3 +5,13 @@ module.exports ={
     USER: 'u917498081_user',
     PASSWORD: 'x1;7RPvCiQ'
 };
+
+//Credenciales de prueba local
+/*
+module.exports ={
+    HOST: 'localhost',
+    DB: 'devAPI',
+    USER: 'root',
+    PASSWORD: ''
+};
+*/
